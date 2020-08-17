dd80aed552291f26c88eb7d490bc7607d3e36f77
Victor Mourão <pvictormourao@gmail.com>
2020-08-16 22:52:57 -0300

    REFACTOR: Updating Dashboard page to perform better

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -23,6 +23,11 @@ interface Transaction {
   created_at: Date;
 }
 
+interface UnformattedBalance {
+  income: number;
+  outcome: number;
+  total: number;
+}
 interface Balance {
   income: string;
   outcome: string;
@@ -37,12 +42,27 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
       const response = await api.get<{
-        balance: Balance;
+        balance: UnformattedBalance;
         transactions: Transaction[];
       }>('/transactions');
-      const downloadedBalance: Balance = response.data.balance;
-      const downloadedTransactions: Transaction[] = response.data.transactions;
-      setBalance(downloadedBalance);
+      const downloadedBalance = response.data.balance;
+      const downloadedTransactions: Transaction[] = response.data.transactions.map(
+        transaction => {
+          return {
+            ...transaction,
+            formattedDate: new Date(
+              transaction.created_at,
+            ).toLocaleDateString(),
+            formattedValue: formatValue(transaction.value),
+          };
+        },
+      );
+      const formattedBalance: Balance = {
+        income: formatValue(downloadedBalance.income),
+        outcome: formatValue(downloadedBalance.outcome),
+        total: formatValue(downloadedBalance.total),
+      };
+      setBalance(formattedBalance);
       setTransactions(downloadedTransactions);
     }
 
@@ -59,7 +79,7 @@ const Dashboard: React.FC = () => {
             {formatValue(transaction.value)}
           </td>
           <td>{transaction.category.title}</td>
-          <td>{new Date(transaction.created_at).toLocaleDateString()}</td>
+          <td>{transaction.formattedDate}</td>
         </tr>
       ));
     }
@@ -76,30 +96,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">
-              {formatValue(Number(balance.income))}
-            </h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">
-              {formatValue(Number(balance.outcome))}
-            </h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">
-              {Number(balance.total).toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-              })}
-            </h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
